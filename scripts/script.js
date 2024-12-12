1e9819d8614e0f01035b6531e88d1b6738b64a1f
Olivier Nadeau <oliviernadeau2003@gmail.com>
2024-12-11 21:28:20 -0500

    .

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const POUCESVERSCM = 2.5;
 const CMVERSPOUCES = 1 / 2.5;
 
 function poucesVersCm(valeur) {
-    document.getElementById("resultat").innerHTML = "Pouces => " + (valeur * POUCESVERSCM).toFixed(1) + " cm";
+    document.getElementById("resultat").innerHTML = "pouces => " + (valeur * POUCESVERSCM).toFixed(1) + " cm";
 }
 
 function cmVersPouces(valeur) {
@@ -23,7 +23,43 @@ function afficherPartie2(nomPrenom, taille) {
     if (!(taille >= 1 && taille <= 6))
         document.getElementById("resultat2").innerHTML = "<span class='incorrect'>Erreur le niveau doit être entre 1 et 6</span>";
     else
-        document.getElementById("resultat2").innerHTML = "<h" + taille + " class='correct'>Bonjour " + nomPrenom + " Niveau= " + taille + "</h" + taille + " > ";
+        document.getElementById("resultat2").innerHTML = "<p><h" + taille + " class='correct'>Bonjour " + nomPrenom + " Niveau= " + taille + "</h" + taille + " ></p> ";
 }
 
 // Partie 03
+
+var monMenu = {
+    "Escargot": { cat: "Entree", image: "escargot.jpg", prix: 5.50 },
+    "Salade verte": { cat: "Entree", image: "salade2.jpg", prix: 5.95 },
+    "Salade César": { cat: "Entree", image: "salade.jpg", prix: 6.95 },
+    "Spaghetti": { cat: "Principal", image: "spaghetti.jpg", prix: 15.95 },
+    "Steak": { cat: "Principal", image: "steak.jpg", prix: 25.95 },
+    "Pizza": { cat: "Principal", image: "pizza.jpg", prix: 17.95 },
+    "Crème glacée": { cat: "Dessert", image: "cremeglacee.jpg", prix: 4.25 },
+    "Gateau": { cat: "Dessert", image: "gateau.jpg", prix: 4.95 },
+    "Pouding": { cat: "Dessert", image: "pouding.jpg", prix: 3.95 },
+    "Café / Thé": { cat: "Boisson", image: "cafe.jpg", prix: 2.50 },
+    "Boisson gazeuse": { cat: "Boisson", image: "boisson.jpg", prix: 2.95 }
+};
+
+$(document).ready(function () {
+
+    //Affichage de la liste niveau 1 et 2
+    $('li').on('click', function (e) {
+        if ($(this).hasClass('elementSelec'))
+            return;
+        let selelement = $(this).parent();
+        selelement.find('li').removeClass('elementSelec').find('ul').hide();
+        $(this).addClass('elementSelec').children('ul').slideToggle(500);
+    });
+
+    $('li li').on('click', function (e) {
+        let selectMenu = $(this).html();
+        alert(" Vous avez cliquez :" + selectMenu);
+        let varprixAccueil = $("#prixAccueil").html();
+        if (isNaN(varprixAccueil))
+            varprixAccueil = 0;
+        alert("Le montant total avec taxes des formations choisites est de : " + parseFloat((varprixAccueil) * 1.15) + " $");
+
+    });
+});
